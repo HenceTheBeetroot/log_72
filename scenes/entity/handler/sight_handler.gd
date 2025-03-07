cd8991e4bc3417c1c52711ce09d155690ee46d9f
HenceTheBeetroot <tomaskovicjesse@gmail.com>
2025-03-07 16:08:08 -0500

    Mar-7-2025 commit

--- scenes/entity/handler/sight_handler.gd
+++ scenes/entity/handler/sight_handler.gd
@@ -3,19 +3,21 @@ class_name SightHandler
 
 @export var FOV: float = 90
 @export var DISTANCE: float = 300
-@export var CASTS: int = 24
+@onready var area_2d = $Area2D
+@onready var circle = $Area2D/CollisionShape2D
 
 func _ready() -> void:
 	FOV = deg_to_rad(FOV)
-	generate_raycasts()
+	generate_field()
 
-func generate_raycasts() -> void:
-	for i in CASTS:
-		var ray: RayCast2D = RayCast2D.new()
-		var angle: float = FOV * ((i - ((CASTS - 1.0) / 2.0)) / (CASTS - 1.0))
-		ray.target_position = Vector2.UP.rotated(angle) * DISTANCE
-		add_child(ray)
-		ray.enabled = true
+func generate_field() -> void:
+	circle.scale = DISTANCE
+	#for i in CASTS:
+	#	var ray: RayCast2D = RayCast2D.new()
+	#	var angle: float = FOV * ((i - ((CASTS - 1.0) / 2.0)) / (CASTS - 1.0))
+	#	ray.target_position = Vector2.UP.rotated(angle) * DISTANCE
+	#	add_child(ray)
+	#	ray.enabled = true
 
 func _process(delta: float) -> void:
 	pass
